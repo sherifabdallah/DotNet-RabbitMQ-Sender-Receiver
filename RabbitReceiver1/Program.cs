dc50ec66719ed3257b4a2d2cf01fcc2ec1b01a69
sherifabdallah <sherif.abdullah.dev@gmail.com>
2024-04-22 22:07:42 +0200

    first commit

---
+++ RabbitReceiver1/Program.cs
@@ -0,0 +1,44 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "user", Password = "mypass" };
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+
+
+        string queueName = "hello";
+
+        channel.QueueDeclare(queue: queueName,
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var response = channel.QueueDeclarePassive(queueName);
+        Console.WriteLine($"Messages ready state in the queue: {response.MessageCount}");
+        
+
+        var consumer = new EventingBasicConsumer(channel);
+        consumer.Received += (model, ea) =>
+        {            
+            var body = ea.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body);
+            Console.WriteLine($"Received {message}");
+        };
+        channel.BasicConsume(queue: queueName,
+                             autoAck: true,
+                             consumer: consumer);
+
+        Console.WriteLine("Press [enter] to exit.");
+        Console.ReadLine();
+
+    }
+}
---
+++ RabbitSender/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear();
+
+        string queueName = "testQueue";
+
+        var factory = new ConnectionFactory() { HostName = "localhost", Port = 5672, UserName = "user", Password = "mypass" };
+        var connection = factory.CreateConnection();
+        var channel = connection.CreateModel();
+        channel.QueueDeclare(queue: queueName,
+                             durable: false,
+                             exclusive: false,
+                             autoDelete: false,
+                             arguments: null);
+
+        var response = channel.QueueDeclarePassive(queueName);
+
+        Console.WriteLine($"Number of Consumers the queue has & Connected Right Now: {response.ConsumerCount}");
+
+        Console.Write("Enter the Message Id: ");
+        string? messageNumber = Console.ReadLine();
+
+        string message = $"Message #{messageNumber}";
+        Console.WriteLine(message);
+        var body = Encoding.UTF8.GetBytes(message);
+
+        // channel.
+        channel.BasicPublish(exchange: "",
+                             routingKey: queueName,
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine($"Sent {message}");
+    }
+}
